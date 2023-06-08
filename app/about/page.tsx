f8f5c826f0e9f05670434a4049148e232b7fd4bc
Ty <ty.jvile@gmail.com>
2023-06-08 14:35:43 +0930

    finish chapter 1

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+export default function About() {
+  // throw new Error("About Error");
+  return (
+    <>
+      <Link href="/">Link home</Link>
+      <h1>About Page</h1>;
+    </>
+  );
+}
